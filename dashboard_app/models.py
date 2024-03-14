6527bfe20e6a965576005d2f5dc96b8ead68bd79
Eduardo Salinas <eduardoluis93@gmail.com>
2024-03-14 18:49:46 +0000

    BUGFIX: I can now insert Child Pages within the Product Index Page in the Wagtail Admin panel. However, I still can't add child pages to the Dashboard Index Page.

--- dashboard_app/models.py
+++ dashboard_app/models.py
@@ -283,6 +283,24 @@ class DashboardIndexPage(Page):
     ]
 
 
+class DashboardPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+    ]
+
+
+
 """ Product Index Page. This is the "view" that renders the page that displays the List of Products.
 
 Here's a general algorithm to add pagination to your Wagtail page:
