7b1d518d76639000b6a9912725f207ad836bca8d
Eduardo Salinas <eduardoluis93@gmail.com>
2024-03-18 22:10:13 +0000

    BUGFIX: I'm finally rendering a Form from a Forms.py file by using a Page made with Wagtail in models.py! However, right now, the form isn't sending anything when you submit it.

--- dashboard_app/models.py
+++ dashboard_app/models.py
@@ -274,7 +274,7 @@ from django.urls import reverse
 # from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
 # from wagtail.contrib.forms.views import process_form_submission
-from .forms import AddArtworkForm
+from .forms import AddArtworkForm, AddProductForm
 from django.core.exceptions import ObjectDoesNotExist
 import datetime
 
@@ -495,6 +495,88 @@ class ArtworkPageGalleryImage(Orderable):
         FieldPanel('caption'),
     ]
 
+
+""" In this code, `ProductRegistrationPage` is a new `Page` model that represents the product registration page. The
+`serve` method is overridden to handle the form submission. If the form is valid, the form handling logic
+is executed and the user is redirected to the same page. If the form is invalid, the form with error messages is
+rendered.
+
+Please note that you need to create a new template `product_registration_page.html` for this page. The form and formset
+are passed to the template context, so you can render them in the template.
+
+Also, please note that the `login_required` decorator is not used in Wagtail. Instead, you can use Wagtail's privacy
+settings to restrict access to the page. You can set the page to be viewable by logged in users only
+in the Wagtail admin interface.
+
+Besides the fact that I was putting "models.field_type" instead of "forms.field_type" in forms.py, the other error that 
+was causing my form fields to not render was that "context" had to have this notation:
+    context['jinja_variable_with_form'] = jinja_variable_with_form
+"""
+
+
+class ProductRegistrationPage(AbstractEmailForm):
+    intro = models.CharField(max_length=255, blank=True)
+    thank_you_text = models.CharField(max_length=255, blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel('intro'),
+        FieldPanel('thank_you_text'),
+    ]
+
+    # Form for creating a new Artwork Page
+    main_form = AddProductForm()
+
+    # Serve() function. This will execute traditional Django code. This will send the form and detect if the user
+    # made a POST request (sent the form).
+    def serve(self, request, *args, **kwargs):
+
+        # If the user submits the form via a POST request
+        if request.method == 'POST':
+
+            # This declares the form, which is imported from forms.py
+            main_form = AddProductForm(request.POST, request.FILES)
+
+            # formset = FormsetImagenesSecundariasParaUnProducto(request.POST, request.FILES)
+
+            # if form.is_valid() and formset.is_valid():
+
+            # If the form is sanitized and valid
+            if main_form.is_valid():
+
+                # Handle the form/formset submission
+                # This is where you put the logic that was in your Django view
+                # ...
+
+                # Redirect to the same page after form submission:
+                # return HttpResponseRedirect(reverse('product_registration_page'))
+                # This renders the page with the form
+                context = self.get_context(request)
+
+                # This should send the Form via a Jinja variable to the template
+                context['form'] = form
+                return render(request, 'dashboard_app/products/product_registration_page.html', context)
+            else:
+                # If the form is invalid, render the form with error messages
+                return self.render_landing_page(request, form, *args, **kwargs)
+        else:
+
+            # If the user first enters the page, this will render the Form
+
+            # Form for creating a new Artwork Page
+            main_form = AddProductForm()
+
+            # formset = FormsetImagenesSecundariasParaUnProducto()
+
+        context = self.get_context(request)
+
+        # This should send the Form via a Jinja variable to the template. The "main_form" is the Jinja variable that
+        # contains the form. Both the word inside the brackets and the word after the equal sign must be the same.
+        context['main_form'] = main_form
+
+        # context['formset'] = formset
+
+        # This renders the page with the form
+        return render(request, 'dashboard_app/products/product_registration_page.html', context)
 
 # """ Form fields.
 #
